c19d8b0d4b769b653add920437095274690841fd
Muhammad Fakhri <muhammadfakhri301@gmail.com>
2021-02-25 16:07:54 +0700

    [Feature] First commit

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Serve index / route
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->successResponse(["version" => app()->version(), "servertime" => time()], "Welcome to Nyxordinal Online Notes API");
+    }
+
+    /**
+     * Get note
+     * @param string $note_id
+     * @return Response
+     */
+    public function get($note_id)
+    {
+        $note = Note::firstOrCreate(
+            ["title" => $note_id],
+            ["content" => ""]
+        );
+        return $this->successResponse(["note" => $note]);
+    }
+
+    /**
+     * Update note
+     * @param  Request  $request
+     * @return Response
+     */
+    public function update(Request $request)
+    {
+        $this->validate($request, [
+            'note_id' => 'required',
+            'content' => 'required'
+        ]);
+
+        $note = Note::updateOrCreate(
+            ["title" => $request->input('note_id')],
+            ["content" => $request->input('content')]
+        );
+
+        return $this->successResponse(["note" => $note]);
+    }
+}
